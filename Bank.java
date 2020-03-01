fff090115cf7cc6e2748e1ed3ca39a2da6abc303
priyanka <60843754+priya6465@users.noreply.github.com>
2020-03-01 15:42:01 +0530

    Create Bank.java
    new p

---
+++ Bank.java
@@ -0,0 +1,25 @@
+
+
+public class Bank {
+	
+	public static int get_sbi(){
+		return 45;
+		}
+	public static int get_icici(){
+		return 88;
+		}
+	public static int get_yesbank(){
+		return 99;
+	}
+	
+	public static void main(String[] args)
+	{
+		System.out.println("value of sbi"+ get_sbi());
+		System.out.println("value of icici"+ get_icici());
+		System.out.println("value of yesbank"+ get_yesbank());
+			
+		}
+		
+	
+
+}
