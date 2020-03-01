2ff9533d97f7a3061976a99668734a8af4f3a78f
Priyanka <p.priyapatil579@gmail.com>
2020-03-01 15:32:03 +0530

    modifyed4

--- Verifytiltle.java
+++ Verifytiltle.java
@@ -5,13 +5,14 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class Verifytiltle {
-@Test
-public void VerifyHomePageTitle()
-{
-	WebDriver driver=new ChromeDriver();
-	driver.navigate().to("");
-	
-}
-	
-
-}
+	public static void main(String[] args) throws InterruptedException {
+		 //system setting Chrome
+	System.setProperty("webdriver.chrome.d"+ "river", "F:\\selenium software\\chromedriver_win32\\chromedriver.exe");
+		//launch ff browser
+	 WebDriver driver=new ChromeDriver();
+	 //launch google
+	 driver.get("https://www.google.com/");
+	 //browser close
+	 Thread.sleep(5000);
+	 driver.close();
+	}}
